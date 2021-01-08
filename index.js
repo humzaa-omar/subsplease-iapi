b4d55d7e2ee9e6dcf1b3dc83ab92d25e0bbafff1
hfomar-nafcs <hfomar@student.nafcs.org>
2021-01-08 18:04:03 -0500

    changed /magnet and /torrent to /feed/magnet & /feed/torrent

--- index.js
+++ index.js
@@ -10,42 +10,42 @@ var syntax = require("./syntax.json");
 app.get("/api/v1", function (req, res) {
   res.json(syntax);
 });
-app.get("/api/v1/magnet/1080p", function (req, res) {
+app.get("/api/v1/feed/magnet/1080p", function (req, res) {
   parser.parseURL("https://subsplease.org/rss/?r=1080", function (err, feed) {
     res.json(feed);
   });
 });
-app.get("/api/v1/magnet/720p", function (req, res) {
+app.get("/api/v1/feed/magnet/720p", function (req, res) {
   parser.parseURL("https://subsplease.org/rss/?r=720", function (err, feed) {
     res.json(feed);
   });
 });
-app.get("/api/v1/magnet/sd", function (req, res) {
+app.get("/api/v1/feed/magnet/sd", function (req, res) {
   parser.parseURL("https://subsplease.org/rss/?r=sd", function (err, feed) {
     res.json(feed);
   });
 });
-app.get("/api/v1/magnet/all", function (req, res) {
+app.get("/api/v1/feed/magnet/all", function (req, res) {
   parser.parseURL("https://subsplease.org/rss", function (err, feed) {
     res.json(feed);
   });
 });
-app.get("/api/v1/torrent/1080p", function (req, res) {
+app.get("/api/v1/feed/torrent/1080p", function (req, res) {
   parser.parseURL("https://subsplease.org/rss/?t&r=1080", function (err, feed) {
     res.json(feed);
   });
 });
-app.get("/api/v1/torrent/720p", function (req, res) {
+app.get("/api/v1/feed/torrent/720p", function (req, res) {
   parser.parseURL("https://subsplease.org/rss/?t&r=720", function (err, feed) {
     res.json(feed);
   });
 });
-app.get("/api/v1/torrent/sd", function (req, res) {
+app.get("/api/v1/feed/torrent/sd", function (req, res) {
   parser.parseURL("https://subsplease.org/rss/?t&r=sd", function (err, feed) {
     res.json(feed);
   });
 });
-app.get("/api/v1/torrent/all", function (req, res) {
+app.get("/api/v1/feed/torrent/all", function (req, res) {
   parser.parseURL("https://subsplease.org/rss?t", function (err, feed) {
     res.json(feed);
   });
