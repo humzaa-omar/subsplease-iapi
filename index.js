9b47f4da1183b409fe9bb82a38edf2739358858b
hfomar-nafcs <hfomar@student.nafcs.org>
2021-01-08 13:01:31 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,59 @@
+#!/usr/bin/env node
+
+var Parser = require('rss-parser');
+var parser = new Parser();
+var express = require("express");
+var app = express();
+
+app.get('/api/v1/magnet/1080p',function(req,res){
+    parser.parseURL('https://subsplease.org/rss/?r=1080',   function(err, feed) {
+      res.json(feed); 
+      })
+    })
+app.get('/api/v1/magnet/720p',function(req,res){
+    parser.parseURL('https://subsplease.org/rss/?r=720',   function(err, feed) {
+      res.json(feed); 
+      })
+    })
+app.get('/api/v1/magnet/sd',function(req,res){
+    parser.parseURL('https://subsplease.org/rss/?r=sd',   function(err, feed) {
+      res.json(feed); 
+      })
+    })
+app.get('/api/v1/magnet/all',function(req,res){
+    parser.parseURL('https://subsplease.org/rss',   function(err, feed) {
+      res.json(feed); 
+      })
+    })
+app.get('/api/v1/torrent/1080p',function(req,res){
+    parser.parseURL('https://subsplease.org/rss/?t&r=1080',   function(err, feed) {
+      res.json(feed); 
+      })
+    })
+app.get('/api/v1/torrent/720p',function(req,res){
+    parser.parseURL('https://subsplease.org/rss/?t&r=720',   function(err, feed) {
+      res.json(feed); 
+      })
+    })
+app.get('/api/v1/torrent/sd',function(req,res){
+    parser.parseURL('https://subsplease.org/rss/?t&r=sd',   function(err, feed) {
+      res.json(feed); 
+      })
+    })
+app.get('/api/v1/torrent/all',function(req,res){
+    parser.parseURL('https://subsplease.org/rss?t',   function(err, feed) {
+      res.json(feed); 
+      })
+    })
+
+
+
+
+
+
+
+    
+app.listen(3000, () => {
+ console.log("Server running on port 3000");
+});
+
