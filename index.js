a98ba5fb516b40145a5c0cf374a2ef722b780c7e
hfomar-nafcs <hfomar@student.nafcs.org>
2021-01-08 13:24:08 -0500

    added Syntax

--- index.js
+++ index.js
@@ -5,7 +5,11 @@ var parser = new Parser();
 var express = require("express");
 var app = express();
 var port = process.env.PORT || 80
+var syntax = `{ "API Version": "v1.0.0", "API Creator": "Humzaa Omar", "Feeds": { "Torrent": { "1080p": "/api/v1/torrent/1080p", "720p": "/api/v1/torrent/720p", "SD": "/api/v1/torrent/sd", "All": "/api/v1/torrent/all" }, "Magnet": { "1080p": "/api/v1/magnet/1080p", "720p": "/api/v1/magnet/720p", "SD": "/api/v1/magnet/sd", "All": "/api/v1/magnet/all" } } }`
 
+app.get('/api/v1/magnet/1080p',function(req,res){
+    res.json(syntax); 
+    })
 app.get('/api/v1/magnet/1080p',function(req,res){
     parser.parseURL('https://subsplease.org/rss/?r=1080',   function(err, feed) {
       res.json(feed); 
